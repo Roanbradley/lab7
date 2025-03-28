a68bffba811c5fbba6869dd2d8f22d2508054502
unknown <roanbradley5@gmail.com>
2025-03-28 13:47:25 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { Component, OnInit} from '@angular/core';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonHeader, IonTitle, IonToolbar, IonContent } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, CommonModule,IonHeader, IonTitle, IonToolbar,IonContent],
 })
-export class HomePage {
-  constructor() {}
+
+
+export class HomePage implements OnInit {
+
+  myMovies:any[] = [];
+  constructor(private ms:MovieService) {}
+
+  ngOnInit(): void {
+    this.ms.getMovieData().subscribe(
+      (data)=>{
+        this.myMovies = data.Search;
+
+
+      }
+    );
+    
+    
+
+
+  }
+
 }
